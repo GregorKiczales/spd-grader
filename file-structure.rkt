d75dca1b8e04dcb6e0ebe6490208a6677d252a8e
Gregor Kiczales <gregork@students.cs.ubc.ca>
2024-01-11 17:17:11 -0800

    move functions having to do with parsing the file, getting elts, elt structure to new file-structure.rkt

---
+++ file-structure.rkt
@@ -0,0 +1,220 @@
+#lang racket
+
+(require "defs.rkt"
+         "utils.rkt")
+
+(provide (all-defined-out))
+
+(struct elt (tags sexp) #:transparent)
+
+(define CHECK-FORMS
+  '(check-expect check-random check-satisfied check-within check-error check-member-of check-range))
+
+
+
+(define (tag-sexps t)
+  (map elt-sexp (filter (compose (curry member t) elt-tags) (elts))))
+
+
+(define (get-problem* n)
+  (or (get-by-pred (lambda (x) (and (@problem? x) (= n (problem-num x)))))
+      (raise-student-error  "Could not find (@problem ~a)." n)))
+
+
+
+(define (get-htdf* n) (get-by-name-or-index '@htdf @htdf? n (lambda (x) (member n (htdf-names x)))))
+(define (get-htdd* n) (get-by-name-or-index '@htdd @htdd? n (lambda (x) (member n (htdd-names x)))))
+(define (get-htdw* n) (get-by-name-or-index '@htdw @htdw? n (lambda (x) (eqv? n (htdw-ws x)))))
+
+(define (get-by-name-or-index kind type? n name?)
+  (cond [(and (symbol? n)
+	      (eqv? #\$ (string-ref (symbol->string n) 0)))
+	 (or (get-by-pred type?)
+	     (raise-student-error "could not find ~a tag." kind))]
+	[(symbol? n)
+	 (or (get-by-pred (lambda (x) (and (type? x) (name? x))))
+	     (raise-student-error "could not find (~a ~a)." kind n))]
+	[(number? n)
+	 (or (get-by-index type? n)
+	     (raise-student-error "could not find ~a number ~a" kind n))]))
+
+(define (get-by-pred p)
+  (let loop ([sexps (if (null? (context)) (map elt-sexp (elts)) (tag-sexps (car (context))))])
+    (cond [(empty? sexps) #f]
+          [(p (car sexps)) (car sexps)]
+          [else
+           (loop (rest sexps))])))
+
+(define (get-by-index p n)
+  (let loop ([i 0]                        ;!!! below was just (elts)
+	     [sexps (if (null? (context)) (map elt-sexp (elts)) (tag-sexps (car (context))))])
+    (cond [(empty? sexps)                 #f]
+	  [(and (p (car sexps)) (= i n))  (car sexps)]
+	  [     (p (car sexps))           (loop (add1 i) (rest sexps))]
+	  [else                           (loop       i  (rest sexps))])))
+
+(define (get-all-tests)
+  (filter check? (map elt-sexp (elts))))
+
+(define (get-function-tests fn-name)
+  (filter (lambda (t)
+	    (and (check? t)
+		 (let ([test-actual (cadr t)])
+		   (and (pair? test-actual)
+			(or (eqv? (car test-actual) fn-name)
+			    (and (eqv? (car test-actual) 'local)
+				 (eqv? (caaddr test-actual) fn-name)))))))
+	  (map elt-sexp (elts))))
+
+
+
+
+
+(define (fn->elts fn)
+  (parse-elts (read-sexps fn)))
+
+(define (read-sexps fn)
+  (call-with-input-file fn
+    (lambda (p)
+      (parameterize ([read-accept-reader #t]
+		     [read-case-sensitive #t]
+                     [read-decimal-as-inexact #f] ;to match teaching languages
+		     [current-input-port p])
+	(read-line (current-input-port))
+	(read-line (current-input-port))
+	(read-line (current-input-port))
+	
+	(let loop ([se (read)])
+	  (if (eof-object? se)
+	      '()
+	      (cons (post-read-convert se) (loop (read)))))))))
+
+(define (read-syntaxes fn)
+  (call-with-input-file fn
+    (lambda (p)
+      (parameterize ([read-accept-reader #t]
+		     [read-case-sensitive #t]
+                     [read-decimal-as-inexact #f] ;to match teaching languages
+		     [current-input-port p])
+
+        (cdr (syntax->list (cadddr (syntax->list (read-syntax fn p)))))))))
+  
+
+
+
+
+(define (parse-elts lose)
+  ;;
+  ;; The grade-xxx forms enforce a nesting of graders as follows
+  ;;  grade-problem (must be at top-level)
+  ;;   grade-htdd
+  ;;     grade-dd-rules-and-template
+  ;;     ...
+  ;;   grade-htdf
+  ;;     grade-signature
+  ;;     grade-tests-validity
+  ;;     ...
+  ;;
+  ;; So we parse the appearance of tags in the file as matching that structure.
+  ;;
+  (define (clear-context @t context)
+    (cond [(eq? @t    '@problem)      (remove-all '(@problem @htdf @htdd) context)]
+          [(member @t '(@htdd @htdf)) (remove-all '(@htdd @htdf) context)]
+          [(eq? @t    '@htdw)         (remove-all '(@htdw) context)]))
+
+  (define (remove-all tags context)
+    (cond [(null? context) context]
+          [else
+           (if (and (pair? (car context))
+                    (member (caar context) tags))
+               (remove-all tags (cdr context))
+               (cons (car context)
+                     (remove-all tags (cdr context))))]))
+           
+
+  (let loop ([lose lose]
+	     [context '()])
+    (if (null? lose)
+	'()
+	(let ([sexp (first lose)])
+	  (cond [(and (pair? sexp)
+		      (member (first sexp) '(@problem @htdw @htdd @htdf)))
+		 (let ([new-context (cons sexp (clear-context (first sexp) context))])
+		   (cons (elt (cdr new-context) sexp)
+			 (loop (cdr lose)
+			       new-context)))]
+                [(and (pair? sexp)
+                      (eqv? (first sexp) '@signature))
+                 (cons (elt context (subst 'false #f sexp)) ;!!! make this go away, false is a symbol in this case not a value
+                       (loop (cdr lose)
+                             context))]
+		[else
+		 (cons (elt context sexp)
+		       (loop (cdr lose)
+			     context))])))))
+
+
+
+(define problem-sexps tag-sexps)  ;some tags have elements after them
+(define htdw-sexps    tag-sexps)
+(define htdd-sexps    tag-sexps)
+(define htdf-sexps    tag-sexps)
+
+(define problem-num             cadr)    ;data in the actual tag
+(define htdw-ws                 cadr)   
+(define htdf-names              cdr)    
+(define htdd-names              cdr)
+(define template-origin-origins cdr)
+(define template-defn           cadr)
+(define dd-template-rules-rules cdr)
+
+;; !!! the remove '@signature is because sometimes this is called on the tag and
+;; !!! sometimes it is called on just the signature. sigh.
+(define (signature-args   sig) (takef (remove '@signature sig) (lambda (x) (not (eqv? x '->))))) ;!!!cdr added because sig includes @signature
+(define (signature-result sig) (cadr (member '-> sig)))
+(define (signature-fail?  sig) (equal? (cddr (member '-> sig)) '(or false)))
+
+(define (problem-htdfs         t) (filter @htdf?              (tag-sexps t)))
+
+(define (htdf-sigs             t) (filter @signature?         (tag-sexps t)))
+(define (htdf-checks           t) (filter check?              (tag-sexps t)))
+(define (htdf-template-origins t) (filter @template-origin?   (tag-sexps t)))
+(define (htdf-templates        t) (filter @template?          (tag-sexps t)))
+(define (htdf-defns            t) (filter fn-defn?            (tag-sexps t)))
+
+(define (htdd-constants        t) (filter const-defn?         (tag-sexps t)))
+(define (htdd-rules            t) (filter @dd-template-rules? (tag-sexps t)))
+(define (htdd-templates        t) (filter fn-defn?            (tag-sexps t)))
+
+
+(define (@assignment?          x) (and (pair? x) (eq? (car x) '@assignment)))
+(define (@cwl?                 x) (and (pair? x) (eq? (car x) '@cwl)))
+
+(define (@problem?             x) (and (pair? x) (eq? (car x) '@problem)))
+
+(define (@htdw?                x) (and (pair? x) (eq? (car x) '@htdw)))
+(define (@htdd?                x) (and (pair? x) (eq? (car x) '@htdd)))
+(define (@htdf?                x) (and (pair? x) (eq? (car x) '@htdf)))
+
+(define (@signature?           x) (and (pair? x) (eq? (car x) '@signature)))
+
+(define (@dd-template-rules?   x) (and (pair? x) (eq? (car x) '@dd-template-rules)))
+(define (@template-origin?     x) (and (pair? x) (eq? (car x) '@template-origin)))
+(define (@template?            x) (and (pair? x) (eq? (car x) '@template)))
+
+(define (require?              x) (and (pair? x) (eq?   (car x) 'require)))
+(define (fn-defn?              x) (and (pair? x) (eq? (car x) 'define) (pair? (cadr x)) (pair? (cddr x))))
+(define (const-defn?           x) (and (pair? x) (eq? (car x) 'define) (symbol? (cadr x))))
+(define (struct-defn?          x) (and (pair? x) (eq? (car x) 'define-struct)))
+(define (defn?                 x) (or (fn-defn? x) (const-defn? x) (struct-defn? x)))
+
+(define (check?                x) (and (pair? x) (member (car x) CHECK-FORMS)))
+(define (check-expect?         x) (and (pair? x) (eq? (car x) 'check-expect)))
+(define (not-check-satisfied?  x) (or (not (pair? x)) (not (eqv? (car x) 'check-satisfied))))
+
+(define fn-defn-name       caadr)
+(define fn-defn-parameters cdadr)
+(define fn-defn-body       caddr)
+
+
+
