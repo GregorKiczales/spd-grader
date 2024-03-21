7344d1981da4d0e7b1e6a7498c1f355c6ae256df
GregorKiczales <38510095+GregorKiczales@users.noreply.github.com>
2024-03-21 13:34:55 -0700

    improve-grade-2-one-of-to-check-whether-question-sub-expressions-are-well-formed

--- 2-one-of.rkt
+++ 2-one-of.rkt
@@ -20,46 +20,87 @@
                         (> (length defns) (sub1 n))
                         (list-ref defns (sub1 n)))])
 
-    (check-2-one-of defn params sol-pairs)))
+    (check-2-one-of htdf defn params sol-pairs)))
 
-(define (check-2-one-of defn params sol-pairs)
-  (ensure-parameter-names params)
-  (let* ([cnd (get-cond defn)]
-         [sub-pairs (and cnd (cdr cnd))])
-    (weights (.1 .2 *)
-      (grade-template-origin (2-one-of))
-      (rubric-item 'template
-                   (and (pair? sub-pairs) (= (length sub-pairs) (length sol-pairs)))
-                   "cond reduced to ~a cases" (length sol-pairs))
-      (if (not (pair? sub-pairs))
-          (rubric-item 'template #f "~a properly simplified questions" (length sol-pairs))
-          (combine-scores
+(define (check-2-one-of htdf defn sol-params sol-pairs)
+  (ensuring
+   (begin
+     (ensure defn                             "(@htdf ~a) must define single top-level function" (cadr htdf))
+     (ensure (pair? (caddr defn))             "~a function definition must be more than stub" (cadr htdf))
+     (ensure (eqv?  (car (caddr defn)) 'cond) "~a function definition body must be a cond expression" (cadr htdf))
+
+     (let* ([cnd         (get-cond defn)]
+            [sub-params  (fn-defn-parameters defn)]
+            [sub-pairs   (cdr cnd)]
+            [sub-qs      (map car sub-pairs)]
+            [sub-sub-qs  (get-sub-qs sub-qs)]
+
+            [sol-qs      (rename-params sub-params sol-params (map car sol-pairs) (fn-defn-name defn))]
+            [sol-sub-qs  (get-sub-qs sol-qs)]
+
+            [invalid-sub-qs (filter (lambda (pq) (not (memf (lambda (opq) (qequal? pq opq)) sol-sub-qs))) sub-sub-qs)])
+
+       (header "2-one-of:"
+         (weights (.1 .4 .2 *)
+           (grade-template-origin (2-one-of))
+           (rubric-item 'template-intact
+                        (null? invalid-sub-qs)
+                        "All sub-expressions in cond questions are well formed~a"
+                        (cond [(null? invalid-sub-qs) ""]
+                              [(null? (cdr invalid-sub-qs))
+                               (format " - ~a is not formed according to template rules" (car invalid-sub-qs))]
+                              [else
+                               (format " - ~a are not formed according to template rules" (list->text invalid-sub-qs))]))
+           (rubric-item 'template-intact
+                        (and (pair? sub-pairs) (= (length sub-pairs) (length sol-pairs)))
+                        "cond expression has ~a cases (question/answer pairs)" (length sol-pairs))
+           (combine-scores
             (weights* 1.0 '(*)
-              (for/list ([n  (in-naturals 0)]
-                         [sol-pair sol-pairs])
-                (rubric-item 'template
-                             (and (> (length sub-pairs) n)
-                                  (qequal? (car (list-ref sub-pairs n))
-                                           (car sol-pair)
-                                           (= n (sub1 (length sol-pairs)))))
+              (for/list ([n     (in-naturals 0)]
+                         [sol-q sol-qs])
+                (rubric-item 'template-intact
+                             (and (pair? sub-qs)
+                                  (> (length sub-qs) n)
+                                  (qequal? (list-ref sub-qs n)
+                                           sol-q))
                              "~a question is properly simplified"
-                             (number->ordinal (add1 n))))))))))
+                             (number->ordinal (add1 n))))))))))))
+
+(define (rename-params sub-params sol-params qs fn-name)
+  (let loop ((sub-params sub-params)
+             (sol-params sol-params)
+             (qs         qs))
+    (cond [(and (null? sub-params) (null? sol-params)) qs]
+          [(null? sub-params) (rubric-item 'template "~a has too few parameters" fn-name)]
+          [(null? sub-params) (rubric-item 'template "~a has too few parameters" fn-name)]
+          [else
+           (loop (cdr sub-params)
+                 (cdr sol-params)
+                 (subst (car sub-params) (car sol-params) qs))])))
+
+;; no need to use walker?
+(define (get-sub-qs qs)
+  (define (for-one-q q)
+    (cond [(not (pair? q)) (list q)]
+          [(member (car q) '(and or not)) (map get-sub-qs (cdr q))]
+          [else (list q)]))
+
+  (foldr append '() (map for-one-q qs)))
+
 
 (define (get-cond defn)
   (if (and (pair? (caddr defn))
            (eqv? (caaddr defn) 'local))
-      (return-cond (caddr (caddr defn)))
-      (return-cond        (caddr defn))))
+      (produce-cond (caddr (caddr defn)))
+      (produce-cond        (caddr defn))))
 
-(define (return-cond x)
+(define (produce-cond x)
   (and (pair? x)
        (eqv? (car x) 'cond)
        x))
 
-
-(define (qequal? sub sol last?)
-  (or (and last? (eqv? sub 'else))
-      (equal? sub sol)
+(define (qequal? sub sol)
+  (or (equal? sub sol)
       (and (pair? sub)
            (pair? sol)
            (member (car sub) '(string=? = equal?))
